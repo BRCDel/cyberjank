0cad30d9fe30042a86dcb83bba8d709532c3a1b2
BRCDel <56025761+brcdel@users.noreply.github.com>
2021-04-15 16:31:07 +0100

    Imported files from old NPP project. Made the meme page more civilised. Cleaned up pages. Rearranged page links.

---
+++ js/messages.js
@@ -0,0 +1,19 @@
+function speakVor(){ 
+	let message = new SpeechSynthesisUtterance("Look at them, they come to this place when they know they are not pure. Tenno use the keys, but they are mere trespassers. Only I, Vor, know the true power of the Void. I was cut in half, destroyed, but through its Janus Key, the Void called to me. It brought me here and here I was reborn. We cannot blame these creatures, they are being led by a false prophet, an impostor who knows not the secrets of the Void. Behold the Tenno, come to scavenge and desecrate this sacred realm. My brothers, did I not tell of this day? Did I not profesize this moment? Now, I will stop them. Now I am changed, reborn through the energy of the Janus Key. Forever bound to the Void. Let it be known, if the Tenno want true salvation, they will lay down their arms, and wait for the baptism of my Janus key. It is time. I will teach these trespassers the redemptive power of my Janus key. They will learn its simple truth. The Tenno are lost, and they will resist. But I, Vor, will cleanse this place of their impurity.");
+	speechSynthesis.speak(message);
+}
+
+function speakNatah(){ 
+	let message = new SpeechSynthesisUtterance("Our history is smoke. Blurred by dreams. Guided by ghosts. A voice, a Void, lurks inside you, its purpose not yet shown. But what am I? My father was a farmer. My mother, a carpenter. Given light by the Golden Lords, to build for them... a better world. But my familys journey was long. Time began to change their light. Creativity. Pride. A will to live. So the Golden wrath came. And after, I was born. A mimic, a spy. Conceived to burrow into nests and swallow the pitch-eggs of their war machine. The Tenno. But when I saw your tender faces, I took mercy. Or so we were told. But in truth, we were both imprisoned in Luas belly. My light remade by the creators. I became a memory, a ghost. Reprogrammed to destroy my family, my people, my history. But now, I am saved. By family. Together, we will overcome the flaws of our light, the Gods of our creation... merging with them, like steel... bearing Amalgams with the weakness of neither.");
+	speechSynthesis.speak(message);
+}
+
+function speakMicrowave(){
+	let message = new SpeechSynthesisUtterance("beep! beep!  beep! unnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn");
+	speechSynthesis.speak(message);
+}
+
+function speakLada(){
+	let message = new SpeechSynthesisUtterance("st st st st st ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo");
+	speechSynthesis.speak(message);
+}
