581bcf8db0b57dd6eec7e68249a8d67969690c22
BRCDel <40444648@live.napier.ac.uk>
2021-04-19 21:20:28 +0100

    Figured out how to update the status window for every choice.

--- js/game.js
+++ js/game.js
@@ -10,7 +10,7 @@ function Path1(){
     document.getElementById("button1").setAttribute("onClick", "Choice1();")
     document.getElementById("button2").setAttribute("onClick", "Choice2();")
     document.getElementById("button3").setAttribute("onClick", "Choice3();")
-
+    document.getElementById("status-paragraph").innerHTML = ('HP: ' + hp + '/' + maxhp + '<br/>ATK: ' + atk + '<br/>DEF: ' + def);
 }
 
 function Path2(){
@@ -19,7 +19,7 @@ function Path2(){
     document.getElementById("button1").setAttribute("onClick", "Choice1();")
     document.getElementById("button2").setAttribute("onClick", "Choice2();")
     document.getElementById("button3").setAttribute("onClick", "Choice3();")
-
+    document.getElementById("status-paragraph").innerHTML = ('HP: ' + hp + '/' + maxhp + '<br/>ATK: ' + atk + '<br/>DEF: ' + def);
 }
 
 function Path3(){
@@ -28,16 +28,20 @@ function Path3(){
     document.getElementById("button1").setAttribute("onClick", "Choice1();")
     document.getElementById("button2").setAttribute("onClick", "Choice2();")
     document.getElementById("button3").setAttribute("onClick", "Choice3();")
+    document.getElementById("status-paragraph").innerHTML = ('HP: ' + hp + '/' + maxhp + '<br/>ATK: ' + atk + '<br/>DEF: ' + def);
 
 }
 function Choice1(){
     alert('Choice 1 performed')
+    document.getElementById("status-paragraph").innerHTML = ('HP: ' + hp + '/' + maxhp + '<br/>ATK: ' + atk + '<br/>DEF: ' + def);
 }
 
 function Choice2(){
     alert('Choice 2 performed')
+    document.getElementById("status-paragraph").innerHTML = ('HP: ' + hp + '/' + maxhp + '<br/>ATK: ' + atk + '<br/>DEF: ' + def);
 }
 
 function Choice3(){
     alert('Choice 3 performed')
+    document.getElementById("status-paragraph").innerHTML = ('HP: ' + hp + '/' + maxhp + '<br/>ATK: ' + atk + '<br/>DEF: ' + def);
 }
