dcdc4a9c5805b033970abd7a1e62eebdfdd21ddb
BRCDel <40444648@live.napier.ac.uk>
2021-04-21 23:57:06 +0100

    Fixed Combat, added Intimidate.

--- js/game.js
+++ js/game.js
@@ -1,20 +1,20 @@
 let hp = 50;
 let maxhp = 50;
-let atk = 10;
-let def = 10;
+let atk = 5;
+let def = 5;
 let lifepath = 'none';
 let last_func = 'none';
 let music = false;
 let escaped = false;
 
-var enemy_hp, enemy_atk, enemy_def;
+var enemy_hp, enemy_atk, enemy_def, fight_won, fight_escaped;
 var audio = new Audio('media/aud/theme_stage1.mp3');
 audio.volume = 0.15;
 audio.loop = true;
 
 function LoadData(){
     if(localStorage.getItem('saved') == 'false' || localStorage.getItem('saved') == null){
-        alert("Generating new game data.");
+        alert("Generating new game data. Don't forget to save often!");
     }else{
         hp = localStorage.getItem('hp');
         maxhp = localStorage.getItem('maxhp');
@@ -119,7 +119,7 @@ function Wanderer_a2(){
 }
 function Wanderer_a3(){
     document.getElementById("game-text").innerHTML = ""
-    document.getElementById("image").setAttribute("src", "media/img/overlook.jpg");
+    document.getElementById("image").setAttribute("src", "media/img/placeholder.jpg");
     document.getElementById("button1").setAttribute("onClick", "Wanderer_a3_a();");
     document.getElementById("button1").innerHTML = "a3_a";
     document.getElementById("button2").setAttribute("onClick", "Wanderer_a3_b();");
@@ -145,24 +145,31 @@ function Shop(last_func){
 /* These function are considered "shared events" between lifepaths and as such will be available to all lifepaths. */
 /* Shared events will be moments where one or more lifepaths will have an advantage. */
 function Alleyway(){
-    document.getElementById("game-text").innerHTML = "As you walk into the alleyway, you come face to face with an armed man. <br/> HP: 25, ATK: 3, DEF: 3";
+    document.getElementById("game-text").innerHTML = "An armed man guards a door at the end of the alleyway.<br/> HP: 25, ATK: 3, DEF: 3";
     document.getElementById("image").setAttribute("src", "media/img/enemy1.jpg");
-    document.getElementById("button1").setAttribute("onClick", "Fight(Alleyway_FightWon, 25, 3, 3);");
+    document.getElementById("button1").setAttribute("onClick", "Fight(Alleyway_Won, Alleyway, 25, 3, 3);");
     document.getElementById("button1").innerHTML = "Attack him";
     if(lifepath == 'Street Rat'){
         document.getElementById("button2").setAttribute("onClick", "SR1();");
         document.getElementById("button2").innerHTML = "[STREET RAT] 'I'm here to see K.'";
     }else{
-        document.getElementById("button2").setAttribute("onClick", "Intimidate();");
-        document.getElementById("button2").innerHTML = "Intimidate him";
+        document.getElementById("button2").setAttribute("onClick", "Intimidate(atk, 3, Alleyway_Won, 'button2');");
+        document.getElementById("button2").innerHTML = "Attempt to intimidate him";
     }
     document.getElementById("button3").setAttribute("onClick", "Run(" + last_func + ");");
     document.getElementById("button3").innerHTML = "Attempt to run away.";
     UpdateStatus('Alleyway');
 }
 
-function Alleyway_FightWon(){
-    alert("Not implemented yet, check back soon!")
+function Alleyway_Won(){
+    document.getElementById("game-text").innerHTML = "With the guard subdued, the door is clear."
+    document.getElementById("image").setAttribute("src", "media/img/alley_door.jpg");
+    document.getElementById("button1").innerHTML = "Go inside";
+    document.getElementById("button1").setAttribute("onClick", "Story_End();");
+    document.getElementById("button2").innerHTML = "Interrogate the guard";
+    document.getElementById("button2").setAttribute("onClick", "Story_End();");
+    document.getElementById("button3").innerHTML = "Move along";
+    document.getElementById("button3").setAttribute("onClick", "Story_End();");
 }
 
 function NeonDistrict(){
@@ -179,7 +186,7 @@ function NeonDistrict(){
 
 /* These are the lifepath-specific, important moments. */
 function SR1(){
-    document.getElementById("game-text").innerHTML = "The man lets you through. You duck through the door at the end of the alley, take the elevator inside and meet K on the rooftop. It looks like something's bothering him."
+    document.getElementById("game-text").innerHTML = "The man lets you through. You duck through the door, take the elevator inside and meet K on the rooftop. It looks like something's bothering him."
     document.getElementById("image").setAttribute("src", "media/img/overlook.jpg");
     document.getElementById("button1").innerHTML = "Ask him what's on his mind.";
     document.getElementById("button1").setAttribute("onClick", "SR1_GreetK();");
@@ -219,50 +226,87 @@ function SR1_BackOut(){
 }
 
 /* These are functions used for game systems. */
-function Fight(before_fight, en_hp, en_atk, en_def){
+function Fight(fightwon, fightescaped, health, attack, defence){
     ChangeTrack("combat_theme");
-    enemy_hp = en_hp;
-    enemy_atk = en_atk;
-    enemy_def = en_def;
-    document.getElementById("game-text").innerHTML = ("Enemy status<br/>HP:" + en_hp + " ATK:" + en_atk + " DEF:" + en_def);
+    enemy_hp = health;
+    enemy_atk = attack;
+    enemy_def = defence;
+    fight_won = fightwon;
+    fight_escaped = fightescaped;
+    document.getElementById("game-text").innerHTML = ("Enemy status<br/>HP:" + health + " ATK:" + attack + " DEF:" + defence);
     document.getElementById("button1").innerHTML = "Attack";
-    document.getElementById("button1").setAttribute("onClick", "Combat();");
-    document.getElementById("button2").innerHTML = "Brace yourself (-50% incoming damage for one attack)";
+    document.getElementById("button1").setAttribute("onClick", "Combat(fight_won, fight_escaped);");
+    document.getElementById("button2").innerHTML = "[NOT IMPLEMENTED] Brace yourself (-50% incoming damage for one attack)";
     document.getElementById("button2").setAttribute("onClick", "Brace();");
     document.getElementById("button3").innerHTML = "Attempt to escape."
     document.getElementById("button3").setAttribute("onClick", "Run(" + last_func + ");");
     if(music){
         EnableMusic();
     }
-    if(en_hp <= 0 || escaped == true){
+    if(enemy_hp <= 0){
         escaped=false;
-        before_fight();
+        fightwon();
+    }else if(escaped == true){
+        fightescaped();
     }
 }
 
-function Combat(){
-    let crit  = Math.floor(Math.random() * 100); //Generate a random number (0-99) to serve as a percentage.
-    crit++ //Increment to make it a number from 1 to 100 instead
-    //15% critical hit chance, exclusive to the player.
+function Combat(win, escape){
+    let crit  = Math.floor(Math.random() * 100);                //Generate a random number (0-99) to serve as a percentage.
+    crit++                                                      //Increment to make it a number from 1 to 100 instead
+                                                                //15% critical hit chance, exclusive to the player.
     if(crit < 16){
         //|dr| is a damage reduction variable that scales with DEF.
         //The player gets 4% damage reduction per point of DEF. Enemies get 3% damage reduction per point of DEF.
         let dr = def * 0.04;
-        hp = hp - (enemy_atk * dr); //Enemies cannot crit.
+        hp = hp - (enemy_atk - (enemy_atk * dr));               //Enemies cannot crit.
+        Math.round(hp);                                         //Round to nearest integer.
         dr = enemy_def * 0.03;
-        enemy_hp = enemy_hp - (2 * atk * dr); //Double damage for critical hits
+        enemy_hp = enemy_hp - (2 * (atk - (atk * dr)));          //Double damage for critical hits
+        Math.round(enemy_hp);                                   //Round to nearest integer.
+        document.getElementById("status-paragraph").innerHTML = ('HP: ' + hp + '/' + maxhp + '<br/>ATK: ' + atk + '<br/>DEF: ' + def);
     }else{
         let dr = def * 0.04;
-        hp = hp - (enemy_atk * dr);
+        hp = hp - (enemy_atk - (enemy_atk * dr));
+        Math.round(hp);                                         //Round to nearest integer
         dr = enemy_def * 0.03;
-        enemy_hp = enemy_hp - (atk * dr); //Same thing but without the double damage.
+        enemy_hp = enemy_hp - (atk - (atk * dr));                //Same thing but without the double damage.
+        Math.round(enemy_hp);                                   //Round to nearest integer
+        document.getElementById("status-paragraph").innerHTML = ('HP: ' + hp + '/' + maxhp + '<br/>ATK: ' + atk + '<br/>DEF: ' + def);
     }
-    document.getElementById("game-text").innerHTML = ("Enemy status<br/>HP:" + en_hp + " ATK:" + en_atk + " DEF:" + en_def);
+    document.getElementById("game-text").innerHTML = ("Enemy status<br/>HP:" + enemy_hp + " ATK:" + enemy_atk + " DEF:" + enemy_def);
+    if(enemy_hp <= 0){
+        Fight(win, escape, enemy_hp, enemy_atk, enemy_def);
+    }else if(hp <= 0){
+        alert("You have died! Loading last save.");
+        LoadData();
+        location.reload();
+    }
+}
+
+function Intimidate(plr_atk, enm_def, win, trigger){
+    // Roll for Intimidation (haha) with a 30% success chance, growing in accordance to the player's Attack against the opponent's Defence.
+    let roll = (Math.floor(Math.random() * 100));
+    roll++;
+    let disparity = plr_atk - enm_def;
+    //If the difference in Attack vs Defence is two digits or bigger, double its effect. Being all scary and stuff.
+    if(disparity < 10){
+        roll = roll + ((disparity) * 0.8);
+    }else{
+        roll = roll + ((disparity) * 1.6);
+    }
+    if(roll >= 70){
+        alert("Intimidation successful.");
+        win();
+    }else{
+        alert("Intimidation failed.");
+        Disable(trigger);
+    }
 }
 
 function Run(f_name){
-    roll = Math.floor(Math.random() * 100); //Generate random number from 0 to 99
-    roll++; //Increment to make it 1 to 100
+    let roll = Math.floor(Math.random() * 100);                 //Generate random number from 0 to 99
+    roll++;                                                     //Increment to make it 1 to 100
     //50% chance of escape
     if(roll > 50){
         alert("Escape successful!");
@@ -276,6 +320,11 @@ function Run(f_name){
         Combat();
         alert("Escape failed!");
     }
+}
+
+function Disable(name){
+    document.getElementById(name).setAttribute("onClick", "");
+    document.getElementById(name).innerHTML = "Not available.";
 }
 
 function UpdateStatus(func){
@@ -311,7 +360,7 @@ function ChangeTrack(track){
 
 /* Debug function. Most likely useless. */
 
-function no_action(last_func){
-    UpdateStatus(last_func);
+function Story_End(){
+    alert("Story implementation ends here for now, stay tuned for updates!");
 }
 
